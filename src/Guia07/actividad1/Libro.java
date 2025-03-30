947c7522956a629dd8742e34ff3c62fcd5f75a1e
GianMCF <gianmarco.castillo@vallegrande.edu.pe>
2025-03-29 23:49:27 -0500

    Libro.java

--- src/Guia07/actividad1/Libro.java
+++ src/Guia07/actividad1/Libro.java
@@ -21,17 +21,4 @@ class Libro {
     public void cambiarAutor(String nuevoAutor) {
         this.autor = nuevoAutor;
     }
-
-    // Método principal para probar la clase
-    public static void main(String[] args) {
-        // Crear un objeto de la clase Libro
-        Libro libro1 = new Libro("1984", "George Orwell");
-
-        // Mostrar detalles del libro
-        System.out.println(libro1.obtenerDetalles());
-
-        // Cambiar autor y mostrar los nuevos detalles
-        libro1.cambiarAutor("Orwell G.");
-        System.out.println(libro1.obtenerDetalles());
-    }
 }
